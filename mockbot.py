ce3b64ea1d5a33e9bb5a48484ae9c6e57ff1266e
Casey Thatsanaphonh <casey.thatsanaphonh@gmail.com>
2025-03-07 12:29:43 -0800

    Added 2 options for mock, user input

--- mockbot.py
+++ mockbot.py
@@ -5,39 +5,48 @@ import os
 intents = discord.Intents.default()
 intents.members = True
 intents.message_content = True
-bot = discord.Bot
-#intents = discord.Intents.default()
+bot = discord.Bot()
 
-# Creating slash command to say hello for testing 
-# /hello
-bot.slash_command(name="hello", description="Say hello to discord")
-async def hello(ctx: discord.ApplicationContext): 
-    await ctx.respond("Hello")
+# Create funtion for mocked text
+def create_mock(msg): 
+    return ''.join(
+            char.lower() if i % 2 == 0 else char.upper()
+            for i, char in enumerate(msg)
+            )
 
 # Creating slash command for mocking 
 # /@user
 @bot.slash_command(name="mock", description="Mock a user you @")
-async def mock(ctx, user: discord.Member):
-    try:
-        # Fetch the last 100 messages in the channel
-        async for message in (ctx.channel.history(limit=100)):
-            print(message)
-            if message.author == user:
-                # Mock the message content
-                mocked_text = ''.join(
-                    char.upper() if i % 2 == 0 else char.lower()
-                    for i, char in enumerate(message.content)
-                )
-                await ctx.respond(f"{user.display_name} says: {mocked_text}")
-                if mocked_text: 
-                    print(f"There is something in here{mocked_text}")
-                return
-        await ctx.respond(f"No recent messages found from {user.display_name} in this channel.")
-    except discord.Forbidden as e:
-        print(f"403 Forbidden Error: {e}")
-        await ctx.respond("I don’t have permission to perform this action. Please check my permissions.")
-    except Exception as e:
-        print(f"Unexpected Error: {e}")
-        await ctx.respond("An error occurred. Please try again later.")
-    
-    bot.run(os.environ["DISCORD_TOKEN"])
+async def mock(
+        ctx: discord.ApplicationContext,
+        user: discord.Option(discord.Member, "The user to mock", required=False), 
+        message: discord.Option(str, "The custom message to mock", required=False), 
+        ):
+    if user:
+
+        try:
+            # Fetch the last 100 messages in the channel
+            async for message in (ctx.channel.history(limit=20)):
+                if message.author == user:
+                    # Mock the message content
+                    mocked_text = create_mock(message.content)
+                    print(f" This is the message: {mocked_text}")
+                    await ctx.respond(f" : {mocked_text}")
+        # Error Checking 
+            await ctx.respond(f"No recent messages found from {user.display_name} in this channel.")
+        except discord.Forbidden as e:
+            print(f"403 Forbidden Error: {e}")
+            await ctx.respond("I don’t have permission to perform this action. Please check my permissions.")
+        except Exception as e:
+            print(f"Unexpected Error: {e}")
+            await ctx.respond("An error occurred. Please try again later.")     
+    # Check if the user entered in a message 
+    elif message: 
+        mocked_message = create_mock(message)
+        print(f"This is the message: {mocked_message}")
+        await ctx.respond(f": {mocked_message}")
+
+@bot.slash_command(name="hello", description="Say hello to the bot")
+async def hello(ctx: discord.ApplicationContext):
+    await ctx.respond("Hey!")
+
